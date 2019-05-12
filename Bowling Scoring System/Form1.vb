c40c0aa3f17b5cc3273e0b293f181366b776a2d0
Lasagnenator <mattaidanrichards@outlook.com>
2019-05-12 15:15:16 +1000

    Moved format things to seperate file. Makes it easier to find and debug.

--- Bowling Scoring System/Form1.vb
+++ Bowling Scoring System/Form1.vb
@@ -3,9 +3,7 @@
     Public TotalPlayers As Integer
     Public CurrentBowl As Integer = 0
     Public Scores As Integer(,) = New Integer(3, 20) {} 'Make the array 4x21 (max scores x players) and Initialise with zeros
-    Public PlayerScoreBoxes As RichTextBox(,) = {
-        {P1F1, P1F2, P1F3, P1F4, P1F5, P1F6, P1F7, P1F8, P1F9, P1F10, Player1TotalScoreBox}
-        }
+    Public PlayerScoreBoxes As RichTextBox(,) = New RichTextBox(0, 10) {}
     Private Enum ValidScores
         Miss
         One
@@ -37,6 +35,9 @@
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         PlayerNumDialog.ShowDialog()
         'ReDim Scores(3, 20) 
+        PlayerScoreBoxes = {
+        {P1F1, P1F2, P1F3, P1F4, P1F5, P1F6, P1F7, P1F8, P1F9, P1F10, Player1TotalScoreBox}
+        }
     End Sub
     Public Sub MakePanels(num As Integer)
         For i = 0 To 4
@@ -87,37 +88,13 @@
             'Game Ended
             MessageBox.Show("Player 1 Won", "Game Over")
         End If
-        'P1F2.Text = FormatScores("a", "b", SubTotal:="cb")
+        'DisplayScore(0, 2, "3", "4", SubTotal:="7")
+        PlayerScoreBoxes(0, 1).Text = FormatOutput.FormatScores("a", "b", SubTotal:="ab")
+        'P1F2.Text = FormatOutput.FormatScores("a", "b", SubTotal:="cb")
     End Sub
 
-    Public Function FormatScores(ByVal Bowl1 As String, ByVal Optional Bowl2 As String = "", ByVal Optional Bowl3 As String = "", ByVal Optional SubTotal As String = Nothing) As String
-        Dim FinalString As String = ""
-        Dim Pad As Integer
-        'No need to use padding as each score is one character long
-        If Bowl2 = "" Then                          'Strike or still on first bowl
-            If Bowl1 = "X" Then                     'Strike
-                FinalString = "      X"             'Make the strike appear in the spot where bowl2 normally goes
-            Else
-                FinalString = Bowl1                 'First bowl only
-            End If
-        Else                                        'Two bowls taken
-            FinalString = Bowl1 + "     " + Bowl2
-        End If
-        If Not (Bowl3 = "") Then                    'Frame 10 third bowl taken
-            FinalString += "     " + Bowl3
-        End If
-        'If Subtotal is given
-        'Padding needed here as scores can be 1,2 or 3 digits long
-        If Not SubTotal = Nothing Then
-            FinalString += vbNewLine + vbNewLine
-            If Not (Bowl3 = "") Then
-                Pad = 9
-            Else
-                Pad = 5
-            End If
-            Pad += SubTotal.Length - 1 'Make it more centered by shifting the padding over by SubTotal.Length-1
-            FinalString += SubTotal.PadLeft(Pad, " ")
-        End If
-        Return FinalString
-    End Function
+    Public Sub DisplayScore(Player As Integer, Frame As Integer, ByVal Bowl1 As String, ByVal Optional Bowl2 As String = "", ByVal Optional Bowl3 As String = "", ByVal Optional SubTotal As String = Nothing)
+        Dim Score As String = FormatOutput.FormatScores(Bowl1, Bowl2, Bowl3, SubTotal)
+        PlayerScoreBoxes(Player, Frame).Text = Score
+    End Sub
 End Class
