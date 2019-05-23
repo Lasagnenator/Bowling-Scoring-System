712f8f03a8f1cd7bc2b39990545804a25e09b591
Lasagnenator <mattaidanrichards@outlook.com>
2019-05-23 16:38:24 +1000

    Added back the properties

--- PlayerPanelControl/PlayerPanelControl.Designer.vb
+++ PlayerPanelControl/PlayerPanelControl.Designer.vb
@@ -27,6 +27,24 @@ Partial Class PlayerPanelControl
         Me.F1 = New System.Windows.Forms.RichTextBox()
         Me.Subtotal1 = New System.Windows.Forms.RichTextBox()
         Me.Total = New System.Windows.Forms.RichTextBox()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.RichTextBox2 = New System.Windows.Forms.RichTextBox()
+        Me.RichTextBox3 = New System.Windows.Forms.RichTextBox()
+        Me.RichTextBox4 = New System.Windows.Forms.RichTextBox()
+        Me.RichTextBox5 = New System.Windows.Forms.RichTextBox()
+        Me.RichTextBox6 = New System.Windows.Forms.RichTextBox()
+        Me.RichTextBox7 = New System.Windows.Forms.RichTextBox()
+        Me.RichTextBox8 = New System.Windows.Forms.RichTextBox()
+        Me.RichTextBox9 = New System.Windows.Forms.RichTextBox()
+        Me.RichTextBox10 = New System.Windows.Forms.RichTextBox()
+        Me.RichTextBox11 = New System.Windows.Forms.RichTextBox()
+        Me.RichTextBox12 = New System.Windows.Forms.RichTextBox()
+        Me.RichTextBox13 = New System.Windows.Forms.RichTextBox()
+        Me.RichTextBox14 = New System.Windows.Forms.RichTextBox()
+        Me.RichTextBox15 = New System.Windows.Forms.RichTextBox()
+        Me.RichTextBox16 = New System.Windows.Forms.RichTextBox()
+        Me.RichTextBox17 = New System.Windows.Forms.RichTextBox()
+        Me.RichTextBox18 = New System.Windows.Forms.RichTextBox()
         Me.MainGroupBox.SuspendLayout()
         Me.TableLayoutPanel1.SuspendLayout()
         Me.SuspendLayout()
@@ -58,6 +76,24 @@ Partial Class PlayerPanelControl
         Me.TableLayoutPanel1.Controls.Add(Me.F1, 0, 0)
         Me.TableLayoutPanel1.Controls.Add(Me.Subtotal1, 0, 1)
         Me.TableLayoutPanel1.Controls.Add(Me.Total, 10, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.RichTextBox1, 1, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.RichTextBox2, 2, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.RichTextBox3, 3, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.RichTextBox4, 4, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.RichTextBox5, 5, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.RichTextBox6, 6, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.RichTextBox7, 7, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.RichTextBox8, 8, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.RichTextBox9, 9, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.RichTextBox10, 1, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.RichTextBox11, 2, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.RichTextBox12, 3, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.RichTextBox13, 4, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.RichTextBox14, 5, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.RichTextBox15, 6, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.RichTextBox16, 7, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.RichTextBox17, 8, 1)
+        Me.TableLayoutPanel1.Controls.Add(Me.RichTextBox18, 9, 1)
         Me.TableLayoutPanel1.Location = New System.Drawing.Point(4, 31)
         Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
         Me.TableLayoutPanel1.RowCount = 2
@@ -72,6 +108,7 @@ Partial Class PlayerPanelControl
         Me.F1.Location = New System.Drawing.Point(3, 3)
         Me.F1.Name = "F1"
         Me.F1.ReadOnly = True
+        Me.F1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None
         Me.F1.Size = New System.Drawing.Size(96, 44)
         Me.F1.TabIndex = 0
         Me.F1.Text = ""
@@ -82,19 +119,220 @@ Partial Class PlayerPanelControl
         Me.Subtotal1.Location = New System.Drawing.Point(3, 53)
         Me.Subtotal1.Name = "Subtotal1"
         Me.Subtotal1.ReadOnly = True
+        Me.Subtotal1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None
         Me.Subtotal1.Size = New System.Drawing.Size(96, 44)
         Me.Subtotal1.TabIndex = 1
         Me.Subtotal1.Text = ""
         '
         'Total
         '
+        Me.Total.BorderStyle = System.Windows.Forms.BorderStyle.None
         Me.Total.Location = New System.Drawing.Point(1023, 3)
         Me.Total.Name = "Total"
         Me.Total.ReadOnly = True
         Me.TableLayoutPanel1.SetRowSpan(Me.Total, 2)
+        Me.Total.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None
         Me.Total.Size = New System.Drawing.Size(97, 94)
         Me.Total.TabIndex = 2
         Me.Total.Text = ""
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.RichTextBox1.Location = New System.Drawing.Point(105, 3)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.ReadOnly = True
+        Me.RichTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None
+        Me.RichTextBox1.Size = New System.Drawing.Size(96, 44)
+        Me.RichTextBox1.TabIndex = 3
+        Me.RichTextBox1.Text = ""
+        '
+        'RichTextBox2
+        '
+        Me.RichTextBox2.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.RichTextBox2.Location = New System.Drawing.Point(207, 3)
+        Me.RichTextBox2.Name = "RichTextBox2"
+        Me.RichTextBox2.ReadOnly = True
+        Me.RichTextBox2.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None
+        Me.RichTextBox2.Size = New System.Drawing.Size(96, 44)
+        Me.RichTextBox2.TabIndex = 4
+        Me.RichTextBox2.Text = ""
+        '
+        'RichTextBox3
+        '
+        Me.RichTextBox3.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.RichTextBox3.Location = New System.Drawing.Point(309, 3)
+        Me.RichTextBox3.Name = "RichTextBox3"
+        Me.RichTextBox3.ReadOnly = True
+        Me.RichTextBox3.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None
+        Me.RichTextBox3.Size = New System.Drawing.Size(96, 44)
+        Me.RichTextBox3.TabIndex = 5
+        Me.RichTextBox3.Text = ""
+        '
+        'RichTextBox4
+        '
+        Me.RichTextBox4.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.RichTextBox4.Location = New System.Drawing.Point(411, 3)
+        Me.RichTextBox4.Name = "RichTextBox4"
+        Me.RichTextBox4.ReadOnly = True
+        Me.RichTextBox4.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None
+        Me.RichTextBox4.Size = New System.Drawing.Size(96, 44)
+        Me.RichTextBox4.TabIndex = 6
+        Me.RichTextBox4.Text = ""
+        '
+        'RichTextBox5
+        '
+        Me.RichTextBox5.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.RichTextBox5.Location = New System.Drawing.Point(513, 3)
+        Me.RichTextBox5.Name = "RichTextBox5"
+        Me.RichTextBox5.ReadOnly = True
+        Me.RichTextBox5.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None
+        Me.RichTextBox5.Size = New System.Drawing.Size(96, 44)
+        Me.RichTextBox5.TabIndex = 7
+        Me.RichTextBox5.Text = ""
+        '
+        'RichTextBox6
+        '
+        Me.RichTextBox6.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.RichTextBox6.Location = New System.Drawing.Point(615, 3)
+        Me.RichTextBox6.Name = "RichTextBox6"
+        Me.RichTextBox6.ReadOnly = True
+        Me.RichTextBox6.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None
+        Me.RichTextBox6.Size = New System.Drawing.Size(96, 44)
+        Me.RichTextBox6.TabIndex = 8
+        Me.RichTextBox6.Text = ""
+        '
+        'RichTextBox7
+        '
+        Me.RichTextBox7.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.RichTextBox7.Location = New System.Drawing.Point(717, 3)
+        Me.RichTextBox7.Name = "RichTextBox7"
+        Me.RichTextBox7.ReadOnly = True
+        Me.RichTextBox7.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None
+        Me.RichTextBox7.Size = New System.Drawing.Size(96, 44)
+        Me.RichTextBox7.TabIndex = 9
+        Me.RichTextBox7.Text = ""
+        '
+        'RichTextBox8
+        '
+        Me.RichTextBox8.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.RichTextBox8.Location = New System.Drawing.Point(819, 3)
+        Me.RichTextBox8.Name = "RichTextBox8"
+        Me.RichTextBox8.ReadOnly = True
+        Me.RichTextBox8.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None
+        Me.RichTextBox8.Size = New System.Drawing.Size(96, 44)
+        Me.RichTextBox8.TabIndex = 10
+        Me.RichTextBox8.Text = ""
+        '
+        'RichTextBox9
+        '
+        Me.RichTextBox9.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.RichTextBox9.Location = New System.Drawing.Point(921, 3)
+        Me.RichTextBox9.Name = "RichTextBox9"
+        Me.RichTextBox9.ReadOnly = True
+        Me.RichTextBox9.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None
+        Me.RichTextBox9.Size = New System.Drawing.Size(96, 44)
+        Me.RichTextBox9.TabIndex = 11
+        Me.RichTextBox9.Text = ""
+        '
+        'RichTextBox10
+        '
+        Me.RichTextBox10.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.RichTextBox10.Location = New System.Drawing.Point(105, 53)
+        Me.RichTextBox10.Name = "RichTextBox10"
+        Me.RichTextBox10.ReadOnly = True
+        Me.RichTextBox10.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None
+        Me.RichTextBox10.Size = New System.Drawing.Size(96, 44)
+        Me.RichTextBox10.TabIndex = 12
+        Me.RichTextBox10.Text = ""
+        '
+        'RichTextBox11
+        '
+        Me.RichTextBox11.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.RichTextBox11.Location = New System.Drawing.Point(207, 53)
+        Me.RichTextBox11.Name = "RichTextBox11"
+        Me.RichTextBox11.ReadOnly = True
+        Me.RichTextBox11.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None
+        Me.RichTextBox11.Size = New System.Drawing.Size(96, 44)
+        Me.RichTextBox11.TabIndex = 13
+        Me.RichTextBox11.Text = ""
+        '
+        'RichTextBox12
+        '
+        Me.RichTextBox12.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.RichTextBox12.Location = New System.Drawing.Point(309, 53)
+        Me.RichTextBox12.Name = "RichTextBox12"
+        Me.RichTextBox12.ReadOnly = True
+        Me.RichTextBox12.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None
+        Me.RichTextBox12.Size = New System.Drawing.Size(96, 44)
+        Me.RichTextBox12.TabIndex = 14
+        Me.RichTextBox12.Text = ""
+        '
+        'RichTextBox13
+        '
+        Me.RichTextBox13.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.RichTextBox13.Location = New System.Drawing.Point(411, 53)
+        Me.RichTextBox13.Name = "RichTextBox13"
+        Me.RichTextBox13.ReadOnly = True
+        Me.RichTextBox13.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None
+        Me.RichTextBox13.Size = New System.Drawing.Size(96, 44)
+        Me.RichTextBox13.TabIndex = 15
+        Me.RichTextBox13.Text = ""
+        '
+        'RichTextBox14
+        '
+        Me.RichTextBox14.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.RichTextBox14.Location = New System.Drawing.Point(513, 53)
+        Me.RichTextBox14.Name = "RichTextBox14"
+        Me.RichTextBox14.ReadOnly = True
+        Me.RichTextBox14.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None
+        Me.RichTextBox14.Size = New System.Drawing.Size(96, 44)
+        Me.RichTextBox14.TabIndex = 16
+        Me.RichTextBox14.Text = ""
+        '
+        'RichTextBox15
+        '
+        Me.RichTextBox15.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.RichTextBox15.Location = New System.Drawing.Point(615, 53)
+        Me.RichTextBox15.Name = "RichTextBox15"
+        Me.RichTextBox15.ReadOnly = True
+        Me.RichTextBox15.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None
+        Me.RichTextBox15.Size = New System.Drawing.Size(96, 44)
+        Me.RichTextBox15.TabIndex = 17
+        Me.RichTextBox15.Text = ""
+        '
+        'RichTextBox16
+        '
+        Me.RichTextBox16.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.RichTextBox16.Location = New System.Drawing.Point(717, 53)
+        Me.RichTextBox16.Name = "RichTextBox16"
+        Me.RichTextBox16.ReadOnly = True
+        Me.RichTextBox16.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None
+        Me.RichTextBox16.Size = New System.Drawing.Size(96, 44)
+        Me.RichTextBox16.TabIndex = 18
+        Me.RichTextBox16.Text = ""
+        '
+        'RichTextBox17
+        '
+        Me.RichTextBox17.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.RichTextBox17.Location = New System.Drawing.Point(819, 53)
+        Me.RichTextBox17.Name = "RichTextBox17"
+        Me.RichTextBox17.ReadOnly = True
+        Me.RichTextBox17.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None
+        Me.RichTextBox17.Size = New System.Drawing.Size(96, 44)
+        Me.RichTextBox17.TabIndex = 19
+        Me.RichTextBox17.Text = ""
+        '
+        'RichTextBox18
+        '
+        Me.RichTextBox18.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.RichTextBox18.Location = New System.Drawing.Point(921, 53)
+        Me.RichTextBox18.Name = "RichTextBox18"
+        Me.RichTextBox18.ReadOnly = True
+        Me.RichTextBox18.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None
+        Me.RichTextBox18.Size = New System.Drawing.Size(96, 44)
+        Me.RichTextBox18.TabIndex = 20
+        Me.RichTextBox18.Text = ""
         '
         'PlayerPanelControl
         '
@@ -114,4 +352,22 @@ Partial Class PlayerPanelControl
     Friend WithEvents F1 As Windows.Forms.RichTextBox
     Friend WithEvents Subtotal1 As Windows.Forms.RichTextBox
     Friend WithEvents Total As Windows.Forms.RichTextBox
+    Friend WithEvents RichTextBox1 As Windows.Forms.RichTextBox
+    Friend WithEvents RichTextBox2 As Windows.Forms.RichTextBox
+    Friend WithEvents RichTextBox3 As Windows.Forms.RichTextBox
+    Friend WithEvents RichTextBox4 As Windows.Forms.RichTextBox
+    Friend WithEvents RichTextBox5 As Windows.Forms.RichTextBox
+    Friend WithEvents RichTextBox6 As Windows.Forms.RichTextBox
+    Friend WithEvents RichTextBox7 As Windows.Forms.RichTextBox
+    Friend WithEvents RichTextBox8 As Windows.Forms.RichTextBox
+    Friend WithEvents RichTextBox9 As Windows.Forms.RichTextBox
+    Friend WithEvents RichTextBox10 As Windows.Forms.RichTextBox
+    Friend WithEvents RichTextBox11 As Windows.Forms.RichTextBox
+    Friend WithEvents RichTextBox12 As Windows.Forms.RichTextBox
+    Friend WithEvents RichTextBox13 As Windows.Forms.RichTextBox
+    Friend WithEvents RichTextBox14 As Windows.Forms.RichTextBox
+    Friend WithEvents RichTextBox15 As Windows.Forms.RichTextBox
+    Friend WithEvents RichTextBox16 As Windows.Forms.RichTextBox
+    Friend WithEvents RichTextBox17 As Windows.Forms.RichTextBox
+    Friend WithEvents RichTextBox18 As Windows.Forms.RichTextBox
 End Class
