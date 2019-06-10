afbe03b71b4f985067671a61eeeb76982994ea2c
Lasagnenator <mattaidanrichards@outlook.com>
2019-06-10 17:47:34 +1000

    Subtotals nearly done. Need to make it add onto the previous subtotals

--- Bowling Scoring System/Form1.vb
+++ Bowling Scoring System/Form1.vb
@@ -1,4 +1,6 @@
-﻿Public Class Form1
+﻿Imports Bowling_Scoring_System.FormatOutput
+
+Public Class Form1
     Public CurrentPlayer As Integer = 0
     Public TotalPlayers As Integer
     Public CurrentFrame As Integer = 0 'Current frame number
@@ -36,7 +38,6 @@
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         PlayerNumDialog.ShowDialog()
         'Then a function to let players input names
-        PlayerPanel1.Scores(0) = 1
     End Sub
     Public Sub MakePanels(ByVal num As Integer)
         For i = 0 To 3
@@ -79,10 +80,11 @@
         If Score = ValidScores.Strike Then 'You got a strike so you don't need the second bowl unless frame 10
             If Not CurrentFrame = 9 Then 'Simulate the player taking a miss next bowl automatically.
                 CurrentFrameBowl += 1
-                DisplayAndUpdateScores(ValidScores.Miss)
-                'SelectPlayer(CurrentPlayer).Frames(CurrentFrame).Scores(CurrentFrameBowl) = "-"
+                'Not sure if need to have this
+                'DisplayAndUpdateScores(ValidScores.Miss)
             End If
         End If
+        'CalculateSubtotals
         UpdatePlayer(CurrentPlayer, Score)
     End Sub
 
@@ -99,7 +101,7 @@
             ElseIf CurrentFrameBowl > 2 Then 'In Frame 10 and bowl 4
                 IncrementNextPlayer()
             ElseIf CurrentFrame = 9 Then 'Did not earn the third bowl in frame 10
-                If Not (FormatOutput.TextToScore(SelectPlayer(CurrentPlayer).Frames(9).Scores(0)) = ValidScores.Strike Or Score = ValidScores.Spare) Then
+                If Not (TextToScore(SelectPlayer(CurrentPlayer).Frames(9).Scores(0)) = ValidScores.Strike Or Score = ValidScores.Spare) Then
                     IncrementNextPlayer()
                 End If
             End If
@@ -132,7 +134,65 @@
     End Sub
     Public Sub DisplayAndUpdateScores(Score As Integer)
         TempScores = SelectPlayer(CurrentPlayer).Frames(CurrentFrame).Scores
-        TempScores(CurrentFrameBowl) = FormatOutput.ScoreToText(Score)
+        TempScores(CurrentFrameBowl) = ScoreToText(Score)
         SelectPlayer(CurrentPlayer).Frames(CurrentFrame).Scores = TempScores
+    End Sub
+    Public Sub CalculateSubtotals()
+        Dim b1 As Integer
+        Dim b2 As Integer
+        Dim b3 As Integer
+        Dim b4 As Integer
+        Dim b5 As Integer
+        For i = 0 To CurrentFrame
+            Dim CFrame = SelectPlayer(CurrentPlayer).Frames(i)
+            Dim Subtotal = 0
+            If i = 9 Then 'bowl1 in frame 10
+                b1 = TextToScore(CFrame.Scores(0)) 'b1f10
+                b2 = TextToScore(CFrame.Scores(1)) 'b2f10
+                b3 = TextToScore(CFrame.Scores(2)) 'b3f10
+                b4 = b5 = 0                        'zeros
+            ElseIf i = 8 Then 'bowl1 in frame 9
+                Dim NFrame = SelectPlayer(CurrentPlayer).Frames(i + 1)
+
+                b1 = TextToScore(CFrame.Scores(0)) 'b1f9
+                b2 = TextToScore(CFrame.Scores(1)) 'b2f9
+                b3 = TextToScore(NFrame.Scores(0)) 'b1f10
+                b4 = TextToScore(NFrame.Scores(1)) 'b2f10
+                b5 = TextToScore(NFrame.Scores(2)) 'b3f10
+            Else 'bowl1 in frame 1-8
+                Dim NFrame = SelectPlayer(CurrentPlayer).Frames(i + 1)
+                Dim AFrame = SelectPlayer(CurrentPlayer).Frames(i + 2)
+
+                b1 = TextToScore(CFrame.Scores(0)) 'b1fi
+                b2 = TextToScore(CFrame.Scores(1)) 'b2fi
+                b3 = TextToScore(NFrame.Scores(0)) 'b1fi+1
+                b4 = TextToScore(NFrame.Scores(1)) 'b2fi+1
+                b5 = TextToScore(AFrame.Scores(0)) 'b1fi+2
+            End If
+
+            If i = 9 Then 'bowl 1 in frame 10
+                If ((b1 = b2) And (b2 = ValidScores.Strike)) Then 'b1=b2=strike
+                    Subtotal = b1 + (2 * b2) + (3 * b3)
+                ElseIf b1 = ValidScores.Strike Then
+                    Subtotal = b1 + (2 * (b2 + b3))
+                ElseIf b2 = ValidScores.Spare Then
+                    Subtotal = 10 + (Subtotal * b3)
+                Else
+                    Subtotal = b1 + b2
+                End If
+            Else
+                If (b1 = b3) And (b3 = b4) And (b4 = ValidScores.Strike) Then 'b1=b3=b4=strike
+                    Subtotal = b1 + b3 + b4
+                ElseIf (b1 = b3) And (b3 = ValidScores.Strike) Then 'b1=b3=strike
+                    Subtotal = b1 + b3 + b5
+                ElseIf (b1 = ValidScores.Strike) Then 'b1=strike
+                    Subtotal = b1 + b3 + b4
+                ElseIf (b2 = ValidScores.Spare) Then 'b2=spare
+                    Subtotal = 10 + b3
+                Else
+                    Subtotal = b1 + b2
+                End If
+            End If
+        Next
     End Sub
 End Class
